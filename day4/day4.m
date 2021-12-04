ceca67b8c767bc4a8aba6c9ca310d7bc229c2c12
Jacob Holtom <jacob@holtom.me>
2021-12-03 23:59:03 -0700

    Completed both parts of day 4 in MATLAB

--- day4/day4.m
+++ day4/day4.m
@@ -11,9 +11,9 @@ for i = 1:numel(call_order)
     calls(boards == call) = 1;
     % Find if calls in row or column have hit 5
     [~,c] = find(sum(calls,2) == 5); % Did the column sum hit 5?
-    [~,r] = find(sum(calls) == 5); % Did the row sum hit 5?
+    [~,r] = find(pagetranspose(sum(calls) == 5)); % Did the row sum hit 5?
     if ~isempty(r) % We have a winner!
-        unmarked = sum(boards(abs(1-calls(:,:,r)),r),'all');
+        unmarked = sum(boards(:,:,r).*abs(1-calls(:,:,r)),'all');
         p1 = unmarked*call;
         break;
     elseif ~isempty(c) % We have a winner!
@@ -25,3 +25,28 @@ end
 p1
 
 %% Part 2
+winning_boards = [];
+win_hist = zeros(numel(call_order),size(boards,3));
+calls = zeros(size(boards));
+last_win = Inf;
+for i = 1:numel(call_order)
+    call = call_order(i);
+    % Find all instances of number, mark them as called in calls
+    calls(boards == call) = 1;
+    % Find if calls in row or column have hit 5
+    [~,c] = find(sum(calls,2) == 5); % Did the column sum hit 5?
+    [~,r] = find(pagetranspose(sum(calls) == 5)); % Did the row sum hit 5?
+    if ~isempty(r) || ~isempty(c) % We have a winner!
+        winning_boards = unique([r; c]);
+        win_hist(i,1:numel(winning_boards)) = winning_boards;
+    end
+    if sum(win_hist(i,:) == 0) == 1
+        % Last winner is the only one missing
+        last_win = setdiff(1:size(boards,3),win_hist(i,:));
+    end
+    if ismember(last_win,winning_boards) % This round is the last winning round, compute score.
+        unmarked = sum(boards(:,:,last_win).*abs(1-calls(:,:,last_win)),'all');
+        p2 = unmarked*call
+        return;
+    end
+end
